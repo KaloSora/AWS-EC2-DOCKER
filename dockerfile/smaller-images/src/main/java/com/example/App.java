266af3e5a443a6835ea2dfe70b9216f85d6b7842
Yi Hui Li <1183616932@qq.com>
2025-12-22 21:00:40 +0800

    Commit for docker build testing

---
+++ dockerfile/smaller-images/src/main/java/com/example/App.java
@@ -0,0 +1,9 @@
+package com.example;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello Docker!");
+        System.out.println("Java Version: " + System.getProperty("java.version"));
+        System.out.println("Current time: " + new java.util.Date());
+    }
+}
